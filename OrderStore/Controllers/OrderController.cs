32d87407977fa51e66acc0935d4d7a1d02654c45
Jay Krishna Reddy <jaykrishnareddy@gmail.com>
2021-07-31 14:31:03 +0530

    Swagger Doc Added !

---
+++ OrderStore/Controllers/OrderController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderStore.Domain.Interfaces;
+using System.Threading.Tasks;
+
+namespace OrderStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        // GET: api/<Books>
+        [HttpGet(nameof(GetOrders))]
+        public async Task<IActionResult> GetOrders() => Ok(await _unitOfWork.Orders.GetAll());
+
+        [HttpGet(nameof(GetByGenre))]
+        public IActionResult GetByGenre([FromQuery] string Genre) => Ok(_unitOfWork.Orders.GetOrdersByGenre(Genre));
+    }
+}
